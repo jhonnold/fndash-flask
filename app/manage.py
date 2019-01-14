e6f1a59cc56dd6ddba9de22d6601b0a4713a98df
Jay Honnold <jayhonnold@gmail.com>
2019-01-14 12:48:11 -0600

    Added in basic worker app

--- app/manage.py
+++ app/manage.py
@@ -1,6 +1,6 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import app, db, User
+from main import app, db, User
 
 manager = Manager(app)
 migrate = Migrate(app, db)
