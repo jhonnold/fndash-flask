398e039f8cd512b8ed9f1d34445d796bcf32d28a
Jack Sommer <jack.somm1@gmail.com>
2019-01-19 17:54:14 -0700

    started update from request data

--- app/tasks/worker.py
+++ app/tasks/worker.py
@@ -1,4 +1,4 @@
-import celery, requests
+import celery, requests, json
 
 from app import User
 
@@ -7,4 +7,11 @@ def update_stats():
   users = User.query.all()
   for user in users:
     r = requests.get("https://fortnite-public-api.theapinetwork.com/prod09/users/public/br_stats?user_id={}&platform=pc".format(user.uid))
-    print(r.json())
+    if r.json['stats']['lastmodified_total'] != user.lastmodified_total:
+      user.kills_total = r.json()['stats']['kills_total']
+      user.wins_total = r.json()['stats']['wins_total']
+      user.matchesplayed_total = r.json()['stats']['matchesplayed_total']
+      user.hoursplayed_total = r.json()['stats']['hoursplayed_total']
+      user.lastmodified_total = r.json()['stats']['lastmodified_total']
+      user.kills_solo = r.json()['stats']['kills_solo']
+    print(r.json()['stats'])
