028d14c714cb0437e3431a5c85d5c02d9666a8e4
Jack Sommer <jack.somm1@gmail.com>
2019-01-20 15:59:50 -0700

    added game table

--- app/__init__.py
+++ app/__init__.py
@@ -88,6 +88,13 @@ class User(db.Model):
     def __repr__(self):
         return "<User '{}'>".format(self.username)
 
+class Game(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    uid = db.Column(db.Integer())
+    gametype = db.Column(db.String(6))
+    timeplayed = db.Column(db.Integer())
+    kills = db.Column(db.Integer())
+    placement = db.Column(db.Integer())
 
 @app.route('/')
 def home():
