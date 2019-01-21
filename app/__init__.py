0e4467e1dcfc9b2e509173fca190ea4b09250aa5
Jay Honnold <jayhonnold@gmail.com>
2019-01-21 15:41:03 -0600

    Changes to Game Model
    uid => user_id (more specific and added FK)
    placement => String 'Victory, Top 3 so on'

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_assets import Environment, Bundle
@@ -90,11 +92,14 @@ class User(db.Model):
 
 class Game(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    uid = db.Column(db.Integer())
-    gametype = db.Column(db.String(6))
-    timeplayed = db.Column(db.Integer())
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    game_type = db.Column(db.String(6))
+    time_played = db.Column(db.Integer(), default=datetime.datetime.now)
     kills = db.Column(db.Integer())
-    placement = db.Column(db.Integer())
+    placement = db.Column(db.String())
+
+    def __repr__(self):
+        return "<Game '{}' - '{}'>".format(self.game_type, self.time_played)
 
 @app.route('/')
 def home():
