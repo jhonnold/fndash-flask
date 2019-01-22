d9afa169cbf927751316713fdb91ed89d9959e06
Jay Honnold <jayhonnold@gmail.com>
2019-01-22 17:16:43 -0600

    Updated config to stop printing queries

--- app/config.py
+++ app/config.py
@@ -9,6 +9,6 @@ class ProdConfig(Config):
 class DevConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@postgres/fn-dash-development'
-    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_ECHO = False
     CELERY_BROKER_URL = 'redis://redis:6379/0'
     CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
