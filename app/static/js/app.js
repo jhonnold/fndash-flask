b1a5958b21b15a114142bb4cd5ceaad7407d06aa
Jay Honnold <jayhonnold@gmail.com>
2019-01-22 11:33:12 -0600

    Moved render for all graphs into app.js
    Data is now inject via jinja template, but render occurs later

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -12,6 +12,47 @@ $(document).ready(() => {
     },
   };
 
+  const kdChartOptions = {
+    responsive: true,
+    aspectRatio: 2,
+    legend: {
+      display: false,
+      position: 'bottom',
+      labels: {
+        fontColor: '#fafafa',
+      },
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            min: 0.75,
+            max: 1.5,
+            stepSize: 0.25,
+            fontColor: '#fafafa',
+          },
+          gridLines: {
+            color: 'rgba(255, 255, 255, 0.125)',
+          },
+        },
+      ],
+      xAxes: [
+        {
+          ticks: {
+            fontColor: '#fafafa',
+          },
+        },
+      ],
+    },
+  };
+
+  let kdSolo, kdDuo, kdSquad;
+  let kdAll = new Chart($('#kd-graph-all'), {
+    type: 'line',
+    data: kdAllData,
+    options: kdChartOptions,
+  });
+
   let placementSolo, placementDuo, placementSquad;
   let placementAll = new Chart($('#placement-graph-all'), {
     type: 'pie',
@@ -26,6 +67,13 @@ $(document).ready(() => {
 
     switch (tabId) {
       case 'all_stats':
+        if (kdAll) kdAll.destroy();
+        kdAll = new Chart($('#kd-graph-all'), {
+          type: 'line',
+          data: kdAllData,
+          options: kdChartOptions,
+        });
+
         if (placementAll) placementAll.destroy();
         placementAll = new Chart($('#placement-graph-all'), {
           type: 'pie',
@@ -34,6 +82,13 @@ $(document).ready(() => {
         });
         break;
       case 'solo_stats':
+        if (kdSolo) kdSolo.destroy();
+        kdAll = new Chart($('#kd-graph-solo'), {
+          type: 'line',
+          data: kdSoloData,
+          options: kdChartOptions,
+        });
+
         if (placementSolo) placementSolo.destroy();
         placementSolo = new Chart($('#placement-graph-solo'), {
           type: 'pie',
@@ -42,6 +97,13 @@ $(document).ready(() => {
         });
         break;
       case 'duo_stats':
+        if (kdDuo) kdDuo.destroy();
+        kdDuo = new Chart($('#kd-graph-duo'), {
+          type: 'line',
+          data: kdDuoData,
+          options: kdChartOptions,
+        });
+
         if (placementDuo) placementDuo.destroy();
         placementDuo = new Chart($('#placement-graph-duo'), {
           type: 'pie',
@@ -50,6 +112,13 @@ $(document).ready(() => {
         });
         break;
       case 'squad_stats':
+        if (kdSquad) kdSquad.destroy();
+        kdSquad = new Chart($('#kd-graph-squad'), {
+          type: 'line',
+          data: kdSquadData,
+          options: kdChartOptions,
+        });
+
         if (placementSquad) placementSquad.destroy();
         placementDuo = new Chart($('#placement-graph-squad'), {
           type: 'pie',
