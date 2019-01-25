d2fa04ec089eec6fb0cc5105aeeb092d44503126
Jay Honnold <jayhonnold@gmail.com>
2019-01-25 13:17:54 -0600

    Celery task stores KD data

--- app/tasks/kd_monitor.py
+++ app/tasks/kd_monitor.py
@@ -1,6 +1,24 @@
 import celery
 
+from app import db
+from app.models import User, KD
+
+
 @celery.task()
 def record_stats():
-  logger = record_stats.get_logger();
-  logger.info('Recording stats for all users...')
+    logger = record_stats.get_logger()
+    logger.info('Recording kd stats for all users...')
+
+    users = User.query.all()
+    for user in users:
+        kd = KD(
+            user_id=user.id,
+            total=user.kd_total,
+            solo=user.kd_solo,
+            duo=user.kd_duo,
+            squad=user.kd_squad,
+        )
+
+        db.session.add(kd)
+    
+    db.session.commit()
