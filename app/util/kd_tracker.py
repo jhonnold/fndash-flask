e0c38e955fa22c9341be7ee2dc1763ccc17bfc76
Jay Honnold <jayhonnold@gmail.com>
2019-01-25 22:28:31 -0600

    Modified how the graphs are generated

--- app/util/kd_tracker.py
+++ app/util/kd_tracker.py
@@ -2,14 +2,14 @@ from app.models import KD
 
 
 def kd_progression(user):
-    kds = user.kds.order_by(KD.id.desc()).limit(7)
+    kds = user.kds.order_by(KD.id.desc()).limit(6)
 
-    totals = []
-    solos = []
-    duos = []
-    squads = []
+    totals = [user.kd_total()]
+    solos = [user.kd_solo()]
+    duos = [user.kd_duo()]
+    squads = [user.kd_squad()]
 
-    for i in range(7):
+    for i in range(6):
         try:
             kd = kds[i]
             totals.insert(0, kd.total)
@@ -17,9 +17,9 @@ def kd_progression(user):
             duos.insert(0, kd.duo)
             squads.insert(0, kd.squad)
         except:
-            totals.insert(0, user.kd_total())
-            solos.insert(0, user.kd_solo())
-            duos.insert(0, user.kd_duo())
-            squads.insert(0, user.kd_squad())
+            totals.insert(0, 0)
+            solos.insert(0, 0)
+            duos.insert(0, 0)
+            squads.insert(0, 0)
 
     return (totals, solos, duos, squads)
