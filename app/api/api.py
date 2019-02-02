fb693490bd84b9f46b7527e3945d7e3da2197a54
Jack Sommer <jack.somm1@gmail.com>
2019-02-02 13:13:55 -0700

    added individual user route

--- app/api/api.py
+++ app/api/api.py
@@ -13,3 +13,12 @@ def users():
   serialize_user = lambda user: dict(id=user.id, username=user.username)
   serialized_users = list(map(serialize_user, users))
   return jsonify(serialized_users)
+
+@api.route("/users/<user_id>")
+def user(user_id):
+  user = User.query.filter_by(id=user_id).first()
+  user_data = dict(**user.__dict__)
+  if '_sa_instance_state' in user_data:
+    del user_data['_sa_instance_state']
+
+  return jsonify(user_data)
