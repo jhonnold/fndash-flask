9cda090fdc4266c2b87080c95c7df8c4dcd57fd8
Jay Honnold <jayhonnold@gmail.com>
2019-02-04 13:49:53 -0600

    Created Game and GameList components

--- app/web/src/containers/UserInfo.js
+++ app/web/src/containers/UserInfo.js
@@ -2,24 +2,30 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { actions as userActions } from '../ducks/users';
+import { actions as gamesActions } from '../ducks/games';
 import Stats from '../components/Stats';
+import GameList from '../components/GamesList';
 
 class UserInfo extends React.PureComponent {
   componentDidMount() {
-    const { requestUser, match } = this.props;
+    const { requestUser, match, requestUserGames } = this.props;
 
     requestUser(match.params.userId);
+    requestUserGames(match.params.userId);
   }
 
   componentDidUpdate(prevProps) {
     const { match: prevMatch } = prevProps;
-    const { match, requestUser } = this.props;
+    const { match, requestUser, requestUserGames } = this.props;
 
-    if (match.params.userId !== prevMatch.params.userId) requestUser(match.params.userId);
+    if (match.params.userId !== prevMatch.params.userId) {
+      requestUser(match.params.userId);
+      requestUserGames(match.params.userId);
+    }
   }
 
   render() {
-    const { users, match, ui } = this.props;
+    const { users, match, ui, games } = this.props;
 
     const user = users.data[match.params.userId] || {};
     const data = user[ui.mode] || {};
@@ -28,20 +34,27 @@ class UserInfo extends React.PureComponent {
       <React.Fragment>
         <Stats data={data} />
         <div className="minute-data">
-          <div className="minute-data__container" />
+          <div className="minute-data__container">
+            <div className="games">
+              <GameList title="Records" />
+              <GameList />
+            </div>
+          </div>
         </div>
       </React.Fragment>
     );
   }
 }
 
-const mapStateToProps = ({ users, ui }) => ({
+const mapStateToProps = ({ users, ui, games }) => ({
   users,
   ui,
+  games,
 });
 
 const mapDispatchToProps = dispatch => ({
   ...bindActionCreators(userActions, dispatch),
+  ...bindActionCreators(gamesActions, dispatch),
 });
 
 export default connect(
