4a607296e7635c0fa5f82d4b6d99efdf59d730be
Jay Honnold <jayhonnold@gmail.com>
2019-02-05 12:48:57 -0600

    Added Null check on games iteration

--- app/web/src/components/GamesList.js
+++ app/web/src/components/GamesList.js
@@ -5,7 +5,7 @@ function GamesList({ title = 'Games', games = [] }) {
   return (
     <div className="games__list">
       <h3>{title}</h3>
-      {games.map(g => (
+      {games.map(g => g !== null && (
         <Game key={`${g.time_played} - ${g.game_type}`} data={g} />
       ))}
     </div>
