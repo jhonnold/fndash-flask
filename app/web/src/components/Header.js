e6689c4b43d959a1e19065a9e27b0d459ff00247
Jay Honnold <jayhonnold@gmail.com>
2019-02-04 18:02:43 -0600

    Added in react helmet to change page title

--- app/web/src/components/Header.js
+++ app/web/src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import $ from 'jquery';
+import { Helmet } from 'react-helmet';
 import { actions as usersActions } from '../ducks/users';
 import { actions as uiActions } from '../ducks/ui';
 
@@ -26,43 +27,48 @@ class Header extends React.PureComponent {
     const { username, id } = user;
 
     return (
-      <div className="header">
-        <div className="header__container">
-          <nav className="header__top">
-            <span className="header__title">Fortnite Dashboard</span>
-            <ul className="header__name-menu" data-dropdown-menu>
-              <li>
-                <a href={`/users/${id}`}>{username}</a>
-                <ul className="menu">
-                  {Object.values(data).map(u => (
-                    <li key={u.id}>
-                      <Link to={`/users/${u.id}`}>{u.username}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            </ul>
-          </nav>
-          <div className="header__break" />
-          <div className="header__lower-menu">
-            <span className="header__name">
-              <h2>{username}</h2>
-            </span>
-            <ul className="header__mode-tabs">
-              {['all', 'solo', 'duo', 'squad'].map(v => (
-                <li
-                  key={v}
-                  className={`header__mode-tabs-title ${ui.mode === v ? 'is-active' : ''}`}
-                >
-                  <button type="button" onClick={() => setGameMode(v)}>
-                    {v}
-                  </button>
+      <React.Fragment>
+        <Helmet>
+          <title>{username} Stats - FN Dashboard</title>
+        </Helmet>
+        <div className="header">
+          <div className="header__container">
+            <nav className="header__top">
+              <span className="header__title">Fortnite Dashboard</span>
+              <ul className="header__name-menu" data-dropdown-menu>
+                <li>
+                  <a href={`/users/${id}`}>{username}</a>
+                  <ul className="menu">
+                    {Object.values(data).map(u => (
+                      <li key={u.id}>
+                        <Link to={`/users/${u.id}`}>{u.username}</Link>
+                      </li>
+                    ))}
+                  </ul>
                 </li>
-              ))}
-            </ul>
+              </ul>
+            </nav>
+            <div className="header__break" />
+            <div className="header__lower-menu">
+              <span className="header__name">
+                <h2>{username}</h2>
+              </span>
+              <ul className="header__mode-tabs">
+                {['all', 'solo', 'duo', 'squad'].map(v => (
+                  <li
+                    key={v}
+                    className={`header__mode-tabs-title ${ui.mode === v ? 'is-active' : ''}`}
+                  >
+                    <button type="button" onClick={() => setGameMode(v)}>
+                      {v}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
@@ -77,9 +83,7 @@ const mapDispatchToProps = dispatch => ({
   ...bindActionCreators(uiActions, dispatch),
 });
 
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps,
-  )(Header),
-);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Header);
--- app/web/src/containers/Routes.js
+++ app/web/src/containers/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 import UserInfo from './UserInfo';
 
@@ -7,6 +7,7 @@ const Routes = () => (
   <React.Fragment>
     <Route path="/users/:userId" component={Header} />
     <Route path="/users/:userId" component={UserInfo} />
+    <Route exact path="/" render={() => <Redirect to="/users/1" />} />
   </React.Fragment>
 );
 
