f9d60280b72c6e2352a102c44cb15216b21302b3
Jack <jack.somm1@gmail.com>
2019-03-02 14:54:08 -0700

    Search bar styles

--- app/web/src/components/SearchBar.js
+++ app/web/src/components/SearchBar.js
@@ -6,78 +6,9 @@ import Select from 'react-select';
 import { actions as usersActions } from '../ducks/users';
 import { theme as mainTheme } from '../assets/constants/colors';
 
-const selectStyles = {
-  container: base => ({
-    ...base,
-    fontWeight: '200',
-    color: mainTheme.primary,
-    width: '100%',
-    maxWidth: 240,
-  }),
-  control: base => ({
-    ...base,
-    backgroundColor: mainTheme.black,
-    width: '100%',
-    border: 'none',
-    boxShadow: 'none',
-    cursor: 'text',
-    transition: 'none',
-  }),
-  clearIndicator: base => ({
-    ...base,
-    color: mainTheme.fontColor,
-    cursor: 'pointer',
-    '&:hover': {
-      color: mainTheme.primary,
-    },
-  }),
-  singleValue: base => ({
-    ...base,
-    color: mainTheme.fontColor,
-  }),
-  placeholder: base => ({
-    ...base,
-    color: mainTheme.fontColor,
-  }),
-  input: base => ({
-    ...base,
-    color: mainTheme.primaryFont,
-  }),
-  menu: base => ({
-    ...base,
-    backgroundColor: mainTheme.black,
-    // boxShadow: '0 14px 12px rgba(0,0,0,.25), 0 10px 8px rgba(0,0,0,.22)',
-    borderRadius: 0,
-    border: `1px solid ${mainTheme.white}`,
-    // marginTop: '-0.125rem',
-  }),
-  option: (base, state) => ({
-    ...base,
-    '&:hover': {
-      color: mainTheme.primary,
-      backgroundColor: mainTheme.darkGray,
-    },
-    color: state.isSelected ? mainTheme.primary : mainTheme.fontColor,
-    backgroundColor: mainTheme.black,
-    cursor: 'pointer',
-  }),
-  indicatorSeparator: base => ({
-    ...base,
-    backgroundColor: 'none',
-  }),
-  dropdownIndicator: base => ({
-    ...base,
-    cursor: 'pointer',
-    color: mainTheme.fontColor,
-    '&:hover': {
-      color: mainTheme.primary,
-    },
-  }),
-};
-
 class SearchBar extends React.PureComponent {
   componentDidMount() {
-    const { 
+    const {
       requestUserList,
     } = this.props;
 
@@ -86,9 +17,78 @@ class SearchBar extends React.PureComponent {
 
   render() {
     const {
-      users, goTo,
+      users, goTo, styles,
     } = this.props;
     const { data } = users;
+
+    const selectStyles = {
+      container: base => ({
+        ...base,
+        fontWeight: '200',
+        color: mainTheme.primary,
+        width: '100%',
+        maxWidth: 240,
+      }),
+      control: base => ({
+        ...base,
+        backgroundColor: mainTheme.black,
+        width: '100%',
+        border: 'none',
+        boxShadow: 'none',
+        cursor: 'text',
+        transition: 'none',
+      }),
+      clearIndicator: base => ({
+        ...base,
+        color: mainTheme.fontColor,
+        cursor: 'pointer',
+        '&:hover': {
+          color: mainTheme.primary,
+        },
+      }),
+      singleValue: base => ({
+        ...base,
+        color: mainTheme.fontColor,
+      }),
+      placeholder: base => ({
+        ...base,
+        color: mainTheme.fontColor,
+      }),
+      input: base => ({
+        ...base,
+        color: mainTheme.primaryFont,
+      }),
+      menu: base => ({
+        ...base,
+        backgroundColor: mainTheme.black,
+        // boxShadow: '0 14px 12px rgba(0,0,0,.25), 0 10px 8px rgba(0,0,0,.22)',
+        borderRadius: 0,
+        border: `1px solid ${mainTheme.white}`,
+        // marginTop: '-0.125rem',
+      }),
+      option: (base, state) => ({
+        ...base,
+        '&:hover': {
+          color: mainTheme.primary,
+          backgroundColor: mainTheme.darkGray,
+        },
+        color: state.isSelected ? mainTheme.primary : mainTheme.fontColor,
+        backgroundColor: mainTheme.black,
+        cursor: 'pointer',
+      }),
+      indicatorSeparator: base => ({
+        ...base,
+        backgroundColor: 'none',
+      }),
+      dropdownIndicator: base => ({
+        ...base,
+        cursor: 'pointer',
+        color: mainTheme.fontColor,
+        '&:hover': {
+          color: mainTheme.primary,
+        },
+      }),
+    };
 
     const userOptions = Object.values(data).map(u => ({
       value: u.id,
@@ -98,7 +98,7 @@ class SearchBar extends React.PureComponent {
     return (
       <Select
         options={userOptions}
-        styles={selectStyles}
+        styles={styles || selectStyles}
         onChange={option => goTo(`/users/${option.value}`)}
         isClearable={false}
         value={{ label: userOptions.label, value: userOptions.value }}
--- app/web/src/containers/Homepage.js
+++ app/web/src/containers/Homepage.js
@@ -7,12 +7,14 @@ import GamesList from '../components/GamesList';
 import Column from '../components/Column';
 import SearchBar from '../components/SearchBar';
 import Container from '../components/Container';
+import { theme as mainTheme } from '../assets/constants/colors';
 
 const Banner = styled.div`
   width: 100%;
   border-bottom: 3px solid ${({ theme }) => theme.border};
   text-align: center;
   padding: 2.3rem 0 2rem;
+  background-color: ${({ theme }) => theme.black};
 
   h1 {
     font-size: 50px;
@@ -25,12 +27,84 @@ const Banner = styled.div`
 const HomeContainer = styled(Container)`
   flex-direction: column;
   align-items: center;
-  padding-top: 6rem;
+  padding-top: 8rem;
 `;
 
 const HomeColumn = styled(Column)`
-  margin-top: 15rem;
+  margin-top: 12rem;
 `;
+
+const homeStyles = {
+  container: base => ({
+    ...base,
+    fontWeight: '200',
+    color: mainTheme.primary,
+    width: '100%',
+    maxWidth: 500,
+    height: 65,
+  }),
+  control: base => ({
+    ...base,
+    backgroundColor: mainTheme.black,
+    width: '100%',
+    height: '100%',
+    border: 'none',
+    boxShadow: 'none',
+    cursor: 'text',
+    transition: 'none',
+  }),
+  clearIndicator: base => ({
+    ...base,
+    color: mainTheme.fontColor,
+    cursor: 'pointer',
+    '&:hover': {
+      color: mainTheme.primary,
+    },
+  }),
+  singleValue: base => ({
+    ...base,
+    color: mainTheme.fontColor,
+  }),
+  placeholder: base => ({
+    ...base,
+    color: mainTheme.fontColor,
+  }),
+  input: base => ({
+    ...base,
+    color: mainTheme.primaryFont,
+  }),
+  menu: base => ({
+    ...base,
+    backgroundColor: mainTheme.black,
+    // boxShadow: '0 14px 12px rgba(0,0,0,.25), 0 10px 8px rgba(0,0,0,.22)',
+    borderRadius: 0,
+    border: `1px solid ${mainTheme.white}`,
+    // marginTop: '-0.125rem',
+  }),
+  option: (base, state) => ({
+    ...base,
+    '&:hover': {
+      color: mainTheme.primary,
+      backgroundColor: mainTheme.darkGray,
+    },
+    color: state.isSelected ? mainTheme.primary : mainTheme.fontColor,
+    backgroundColor: mainTheme.black,
+    cursor: 'pointer',
+    fontSize: 25,
+  }),
+  indicatorSeparator: base => ({
+    ...base,
+    backgroundColor: 'none',
+  }),
+  dropdownIndicator: base => ({
+    ...base,
+    cursor: 'pointer',
+    color: mainTheme.fontColor,
+    '&:hover': {
+      color: mainTheme.primary,
+    },
+  }),
+};
 
 class Homepage extends React.PureComponent {
   componentDidMount() {
@@ -54,7 +128,7 @@ class Homepage extends React.PureComponent {
           <h1>FN Dash</h1>
         </Banner>
         <HomeContainer>
-          <SearchBar />
+          <SearchBar styles={homeStyles} />
           <HomeColumn>
             <GamesList games={data} title="Recent Games" />
           </HomeColumn>
