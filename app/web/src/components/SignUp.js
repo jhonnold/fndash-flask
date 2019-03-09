6184d5e3fa769689fada63f482ac0d1e3fbefd26
Jack <jack.somm1@gmail.com>
2019-03-09 09:04:02 -0700

    Better styling

--- app/web/src/components/SignUp.js
+++ app/web/src/components/SignUp.js
@@ -10,19 +10,32 @@ const SignUpContainer = styled(Container)`
   padding: 1rem 0;
   justify-content: center;
   align-items: center;
+`;
 
-  span {
-    font-size: 18px;
-    font-weight: 500;
-    margin-right: 2rem;
-  }
+const Button = styled.button`
+  background-color: ${({ theme }) => theme.white};
+  padding: .5rem 2rem;
+  color: ${({ theme }) => theme.black};
+  border-radius: 4px;
+  cursor: pointer;
 
-  input {
-    border-radius: 4px;
-    height: 2rem;
-    width: 18rem;
-    border: none;
+  &:hover {
+
   }
+`;
+
+const Span = styled.span`
+  font-size: 18px;
+  font-weight: 500;
+  margin-right: 2rem;
+`;
+
+const Input = styled.input`
+  border-radius: 4px;
+  height: 2rem;
+  width: 18rem;
+  border: none;
+  margin-right: 1rem;
 `;
 
 function SignUp() {
@@ -30,8 +43,9 @@ function SignUp() {
     <React.Fragment>
       <Wrap>
         <SignUpContainer>
-          <span>Interested in signing up? Enter your Fortnite User ID!</span>
-          <input name="user_id" value="" placeholder="Enter User ID..." />
+          <Span>Interested in signing up? Enter your Fortnite User ID!</Span>
+          <Input name="user_id" value="" placeholder="Enter User ID..." />
+          <Button type="submit">Join</Button>
         </SignUpContainer>
       </Wrap>
     </React.Fragment>
