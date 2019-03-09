62d3065baafd3513f46cb00b073f8b7cbf8ba983
Jay Honnold <jayhonnold@gmail.com>
2019-03-08 20:18:11 -0600

    RC uses .env file to point to RDS db

--- app/config.py
+++ app/config.py
@@ -1,3 +1,8 @@
+import dotenv, os
+
+dotenv.load_dotenv(verbose=True)
+
+
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
@@ -13,6 +18,19 @@ class ProdConfig(Config):
 class DevConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@postgres/fndash-dev'
+    SQLALCHEMY_ECHO = False
+    CELERY_BROKER_URL = 'redis://redis:6379/0'
+    CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+
+
+class RCConfig(Config):
+    DEBUG = False
+    DB_HOST = os.getenv('DB_HOST')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASS = os.getenv('DB_PASS')
+    DB_NAME = os.getenv('DB_NAME')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:5432/{}'.format(
+        DB_USER, DB_PASS, DB_HOST, DB_NAME)
     SQLALCHEMY_ECHO = False
     CELERY_BROKER_URL = 'redis://redis:6379/0'
     CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
