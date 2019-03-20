69395700d9ff144ad30ead395f919999432b40f2
Jay Honnold <jayhonnold@gmail.com>
2019-03-20 18:11:11 -0500

    Added in client side https forcing
    Fixes #54

--- app/web/src/App.js
+++ app/web/src/App.js
@@ -13,21 +13,38 @@ import { theme } from './assets/constants/colors';
 const history = createHistory();
 const store = configureStore(history);
 
+const isLocalHost = hostname => !!(
+  hostname === 'localhost'
+    || hostname === '[::1]'
+    || hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+);
+
 ReactGA.initialize('UA-136695217-1');
 history.listen((location) => {
-  ReactGA.pageview(location.pathname + location.search);
+  if (!isLocalHost(window.location.hostname)) ReactGA.pageview(location.pathname + location.search);
 });
 
 store.runSaga(rootSaga);
 
-const App = () => (
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <ThemeProvider theme={theme}>
-        <Routes />
-      </ThemeProvider>
-    </ConnectedRouter>
-  </Provider>
-);
+const App = () => {
+  if (
+    window.location
+    && window.location.protocol === 'http:'
+    && !isLocalHost(window.location.hostname)
+  ) {
+    window.location.href = window.location.href.replace(/^http(?!s)/, 'https');
+    return null;
+  }
+
+  return (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <ThemeProvider theme={theme}>
+          <Routes />
+        </ThemeProvider>
+      </ConnectedRouter>
+    </Provider>
+  );
+};
 
 export default hot(module)(App);
