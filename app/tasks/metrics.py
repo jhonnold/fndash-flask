1beb8688f70a5f21e9ee010106356eac3e4ec57f
Jay Honnold <jayhonnold@gmail.com>
2019-03-23 13:43:28 -0500

    Attempting to make metrics work

--- app/tasks/metrics.py
+++ app/tasks/metrics.py
@@ -6,6 +6,25 @@ from boto.utils import get_instance_metadata
 
 logger = get_task_logger(__name__)
 
+
+def send_multi_metrics(instance_id,
+                       region,
+                       metrics,
+                       namespace='EC2/FNDash',
+                       unit='Seconds'):
+    '''
+    Send multiple metrics to CloudWatch
+    metrics is expected to be a map of key -> value pairs of metrics
+    '''
+    cw = cloudwatch.connect_to_region(region)
+    cw.put_metric_data(
+        namespace,
+        metrics.keys(),
+        metrics.values(),
+        unit=unit,
+        dimensions={"InstanceId": instance_id})
+
+
 @celery.task()
 def upload_stat_tracker_metrics(time):
     metadata = get_instance_metadata(timeout=1, num_retries=1)
@@ -14,14 +33,11 @@ def upload_stat_tracker_metrics(time):
     if instance_id is None:
         logger.warn('This is not an EC2 Instance, not uploading stats')
         return
-    
+
     region = metadata.get('placement').get('availability-zone')[0:-1]
 
     app_name = os.environ.get('APP_NAME')
-    metric_name = 'stat_tracker_time_{}'.format(app_name)
+    metric_name = 'StatTrackerTime{}'.format(app_name.captitalize())
     metrics = {metric_name: time}
 
-    cw = cloudwatch.connect_to_region(region)
-    cw.put_metric_data('FN Dash Metrics', metrics.keys(), metrics.values(),
-                       unit='Second',
-                       dimensions={'InstanceId': instance_id})
+    send_multi_metrics(instance_id, region, metrics)
