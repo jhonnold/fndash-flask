cd902c4a2b74b8143a4d1015247f5a1caa40b2a8
Jay Honnold <jayhonnold@gmail.com>
2019-05-26 10:07:00 -0500

    Added flower celery monitor

--- docker/docker.celery.sh
+++ docker/docker.celery.sh
@@ -7,4 +7,5 @@ fi
 
 celery beat -A manage.celery --schedule=/tmp/celerybeat-schedule --loglevel=WARN --pidfile=/tmp/celerybeat.pid &
 celery worker -A manage.celery -P eventlet -c 1000 --loglevel=WARN &
+celery flower -A manage.celery --port=5555 &
 tail -f /dev/null
